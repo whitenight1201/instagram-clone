1750c3c32a79e4fef219f7a13a65cfe6ccfc03c6
whitenight1201 <whitenight1201021@gmail.com>
2023-09-08 08:39:38 -0700

    FE: initial commit and plus tailwindcss

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center w-full h-screen">
+      Hello
+    </div>
+  );
+}
+
+export default App;
